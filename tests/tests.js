5d6e0fbb35a3a2fcaa20748c73ba2bbe3052f658
Yuriy Krutilin <y.krutilin@gmail.com>
2017-11-13 14:18:12 +0300

    recipe exmaple and falling validation
    i got recipe from beerXML repo (https://github.com/brewpoo/BeerXML-Standard/blob/master/examples/MedievalAle.xml) and converted it to the json form. And this processes opened some issues related to the questions, how we plan to store data in the json form. At the moment validation test fails.

--- tests/tests.js
+++ tests/tests.js
@@ -1,31 +1,25 @@
-const rootSchema = require('../json/BeerXML.json')
-const grain = require('../json/grain.json')
-const hops = require('../json/hops.json')
-const mashStep = require('../json/mash_step.json')
-const mash = require('../json/mash.json')
-const measureableUnits = require('../json/measureable_units.json')
-const misc = require('../json/misc.json')
-const recipes = require('../json/recipes.json')
-const style = require('../json/style.json')
-const water = require('../json/water.json')
-const yeast = require('../json/yeast.json')
+const Ajv = require('ajv')
+const ajv = new Ajv()
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'))
+ajv.addSchema(require('../json/grain.json'))
+ajv.addSchema(require('../json/hops.json'))
+ajv.addSchema(require('../json/mash_step.json'))
+ajv.addSchema(require('../json/mash.json'))
+ajv.addSchema(require('../json/measureable_units.json'))
+ajv.addSchema(require('../json/misc.json'))
+ajv.addSchema(require('../json/recipes.json'))
+ajv.addSchema(require('../json/style.json'))
+ajv.addSchema(require('../json/water.json'))
+ajv.addSchema(require('../json/yeast.json'))
 
-var Ajv = require('ajv');
-var ajv = new Ajv();
-ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-ajv.addSchema(grain);
-ajv.addSchema(hops);
-ajv.addSchema(mashStep);
-ajv.addSchema(mash);
-ajv.addSchema(measureableUnits);
-ajv.addSchema(misc);
-ajv.addSchema(recipes);
-ajv.addSchema(style);
-ajv.addSchema(water);
-ajv.addSchema(yeast);
+const validate = ajv.compile(require('../json/BeerXML.json'))
 
-const data = require('./min-valid-recipe.json')
+const testJson = path =>{
+  if (!validate(require(path))) {
+    console.log(validate.errors)
+    process.exit(1)
+  }
+}
 
-var validate = ajv.compile(rootSchema);
-var valid = validate(data);
-if (!valid) console.log(validate.errors);
+testJson('./min-valid-recipe.json')
+testJson('./MedievalAle.json')
