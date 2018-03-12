27099b7d0e6f8e97f1370245aa81c69f34e53d47
Yuriy Krutilin <y.krutilin@gmail.com>
2018-03-12 13:19:05 +0300

    Remove camelCase transformation

--- js/beerxml-to-beerjson.js
+++ js/beerxml-to-beerjson.js
@@ -1,17 +1,5 @@
 const XML = require('pixl-xml')
-
-const camelCase = str =>
-  str.length === 0
-    ? ''
-    : str.length === 1
-      ? str.toLowerCase()
-      : str
-          .replace(/^[_.\- ]+/, '')
-          .toLowerCase()
-          .replace(/[_.\- ]+(\w|$)/g, (m, p1) => p1.toUpperCase())
-
-const xmlToCamelCase = xml =>
-  xml.replace(/<(?!!)(?!\?)[^>]*>/g, str => camelCase(str.toLowerCase()))
+const _ = require('lodash')
 
 const parseBool = s => s === 'TRUE'
 const isBIAB = mashName => mashName.includes('BIAB')
@@ -21,7 +9,7 @@ const dirtyRound = n => Math.round(n * 100000000000) / 100000000000
 
 const importFromBeerXml = xml => {
   try {
-    const recipe = XML.parse(xmlToCamelCase(xml))
+    const recipe = XML.parse(xml)
     console.log(recipe)
     return JSON.stringify(recipe)
   } catch (err) {
