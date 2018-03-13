8499adb6fa98881e4ad3042526817e7b489f0b5c
Yuriy Krutilin <y.krutilin@gmail.com>
2018-03-13 14:29:20 +0300

    Remove some dead code

--- js/beerxml-to-beerjson.js
+++ js/beerxml-to-beerjson.js
@@ -2,10 +2,6 @@ const XML = require('pixl-xml')
 const _ = require('lodash')
 
 const parseBool = s => s === 'TRUE'
-const isBIAB = mashName => mashName.includes('BIAB')
-
-// TODO: May be it is not so good idea. But At the moment I can't figure out best practices for rounding operations.
-const dirtyRound = n => Math.round(n * 100000000000) / 100000000000
 const getArrayNode = node => Array.from(Array.isArray(node) ? node : [node])
 
 const importFromBeerXml = xml => {
@@ -216,7 +212,6 @@ const importFromBeerXml = xml => {
       }
     }
 
-    //console.log(JSON.stringify(recipe))
     return JSON.stringify(recipe)
   } catch (err) {
     console.log('XML Parser Error: ' + err)
