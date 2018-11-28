a438cada59a1577a3f116ebcf8897af6edcc72ce
superroma <roman.eremin@devexpress.com>
2018-11-28 15:17:14 +0300

    Add typescript typings

--- js/gen-docs.js
+++ js/gen-docs.js
@@ -20,13 +20,13 @@ let s = '// @flow\n\n'
 fs.readdirSync(schemaDir).forEach(
   fileName => (s = s + flowConvert(require(schemaDir + fileName)))
 )
-fs.writeFileSync('./flow-typed/beerjson.js', s)
+fs.writeFileSync('./types/flow-typed/beerjson.js', s)
 
 console.log('Generating TypeScript types...')
 
-s = '/// <reference types="angular" />\n\ndeclare namespace BeerJSON {\n'
+s = 'declare namespace BeerJSON {\n'
 fs.readdirSync(schemaDir).forEach(
   fileName => (s = s + tsConvert(require(schemaDir + fileName)))
 )
 s = s + '\n}\n'
-fs.writeFileSync('./typings/beerjson.d.ts', s)
+fs.writeFileSync('./types/ts/beerjson.d.ts', s)
