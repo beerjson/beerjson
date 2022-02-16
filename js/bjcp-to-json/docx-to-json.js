eb3df626bd3e41f58106a528ceba450d7ca70631
Yuriy Krutilin <y.krutilin@gmail.com>
2022-02-16 15:01:15 +0300

    more styles

--- js/bjcp-to-json/docx-to-json.js
+++ js/bjcp-to-json/docx-to-json.js
@@ -466,79 +466,247 @@ const styles = [
     category_id: '23',
     style_id: '23G'
   },
+  {
+    name: 'Witbier',
+    category: 'Belgian Ale',
+    category_id: '24',
+    style_id: '24A'
+  },
+  {
+    name: 'Belgian Pale Ale',
+    category: 'Belgian Ale',
+    category_id: '24',
+    style_id: '24B'
+  },
+  {
+    name: 'Bière de Garde',
+    category: 'Belgian Ale',
+    category_id: '24',
+    style_id: '24C'
+  },
+  {
+    name: 'Belgian Blond Ale',
+    category: 'Strong Belgian Ale',
+    category_id: '25',
+    style_id: '25A'
+  },
+  {
+    name: 'Saison',
+    category: 'Strong Belgian Ale',
+    category_id: '25',
+    style_id: '25B'
+  },
+  {
+    name: 'Belgian Golden Strong Ale',
+    category: 'Strong Belgian Ale',
+    category_id: '25',
+    style_id: '25C'
+  },
 
-  '24. Belgian Ale',
-  '24A. Witbier',
-  '24B. Belgian Pale Ale',
-  '24C. Bière de Garde',
-  '25. Strong Belgian Ale',
-  '25A. Belgian Blond Ale',
-  '25B. Saison',
-  '25C. Belgian Golden Strong Ale',
-  '26. Monastic Ale',
-  '26A. Belgian Single',
-  '26B. Belgian Dubbel',
-  '26C. Belgian Tripel',
-  '26D. Belgian Dark Strong Ale',
-  '27. Historical Beer',
-  'Historical Beer: Kellerbier',
-  'Historical Beer: Kentucky Common',
-  'Historical Beer: Lichtenhainer',
-  'Historical Beer: London Brown Ale',
-  'Historical Beer: Piwo Grodziskie',
-  'Historical Beer: Pre-Prohibition Lager',
-  'Historical Beer: Pre-Prohibition Porter',
-  'Historical Beer: Roggenbier',
-  'Historical Beer: Sahti',
-  'Introduction to Specialty-Type Beer',
-  'Entering Specialty-Type Beers',
-  'Entry Instructions',
-  'Base Style',
-  'Specialty Ingredients',
-  'Best Fit',
-  'Judging Specialty-Type Beers',
-  'Overall Assessment',
-  'Base Style',
-  'Multiple Ingredients',
-  'Balance Effects of Specialty-Type Ingredients',
-  '28. American Wild Ale',
-  '28A. Brett Beer',
-  '28B. Mixed-Fermentation Sour Beer',
-  '28C. Wild Specialty Beer',
-  '28D. Straight Sour Beer',
-  '29. Fruit Beer',
-  '29A. Fruit Beer',
-  '29B. Fruit and Spice Beer',
-  '29C. Specialty Fruit Beer',
-  '29D. Grape Ale',
-  '30. Spiced Beer',
-  '30A. Spice, Herb, or Vegetable Beer',
-  '30B. Autumn Seasonal Beer',
-  '30C. Winter Seasonal Beer',
-  '30D. Specialty Spice Beer',
-  '31. Alternative Fermentables Beer',
-  '31A. Alternative Grain Beer',
-  '31B. Alternative Sugar Beer',
-  '32. Smoked Beer',
-  '32A. Classic Style Smoked Beer',
-  '32B. Specialty Smoked Beer',
-  '33. Wood Beer',
-  '33A. Wood-Aged Beer',
-  '33B. Specialty Wood-Aged Beer',
-  '34. Specialty Beer',
-  '34A. Commercial Specialty Beer',
-  '34B. Mixed-Style Beer',
-  '34C. Experimental Beer',
-  'Argentine Styles',
-  'X1. Dorada Pampeana',
-  'X2. IPA Argenta',
-  'Argentine IPA',
-  'Italian Styles',
-  'X3. Italian Grape Ale',
-  'Brazilian Styles',
-  'X4. Catharina Sour',
-  'New Zealand Styles',
-  'X5. New Zealand Pilsner'
+  {
+    name: 'Belgian Single',
+    category: 'Monastic Ale',
+    category_id: '26',
+    style_id: '26A'
+  },
+  {
+    name: 'Belgian Dubbel',
+    category: 'Monastic Ale',
+    category_id: '26',
+    style_id: '26B'
+  },
+  {
+    name: 'Belgian Tripel',
+    category: 'Monastic Ale',
+    category_id: '26',
+    style_id: '26C'
+  },
+  {
+    name: 'Belgian Dark Strong Ale',
+    category: 'Monastic Ale',
+    category_id: '26',
+    style_id: '26D'
+  },
+
+  //'27. Historical Beer',
+  //'Historical Beer: Kellerbier',
+  //'Historical Beer: Kentucky Common',
+  //'Historical Beer: Lichtenhainer',
+  //'Historical Beer: London Brown Ale',
+  //'Historical Beer: Piwo Grodziskie',
+  //'Historical Beer: Pre-Prohibition Lager',
+  //'Historical Beer: Pre-Prohibition Porter',
+  //'Historical Beer: Roggenbier',
+  //'Historical Beer: Sahti',
+  //'Introduction to Specialty-Type Beer',
+  //'Entering Specialty-Type Beers',
+  //'Entry Instructions',
+  //'Base Style',
+  //'Specialty Ingredients',
+  //'Best Fit',
+  //'Judging Specialty-Type Beers',
+  //'Overall Assessment',
+  //'Base Style',
+  //'Multiple Ingredients',
+  //'Balance Effects of Specialty-Type Ingredients',
+
+  {
+    name: 'Brett Beer',
+    category: 'American Wild Ale',
+    category_id: '28',
+    style_id: '28A'
+  },
+  {
+    name: 'Mixed-Fermentation Sour Beer',
+    category: 'American Wild Ale',
+    category_id: '28',
+    style_id: '28B'
+  },
+  {
+    name: 'Wild Specialty Beer',
+    category: 'American Wild Ale',
+    category_id: '28',
+    style_id: '28C'
+  },
+  {
+    name: 'Straight Sour Beer',
+    category: 'American Wild Ale',
+    category_id: '28',
+    style_id: '28D'
+  },
+  {
+    name: 'Fruit Beer',
+    category: 'Fruit Beer',
+    category_id: '29',
+    style_id: '29A'
+  },
+  {
+    name: 'Fruit and Spice Beer',
+    category: 'Fruit Beer',
+    category_id: '29',
+    style_id: '29B'
+  },
+  {
+    name: 'Specialty Fruit Beer',
+    category: 'Fruit Beer',
+    category_id: '29',
+    style_id: '29C'
+  },
+  {
+    name: 'Grape Ale',
+    category: 'Fruit Beer',
+    category_id: '29',
+    style_id: '29D'
+  },
+  {
+    name: 'Spice, Herb, or Vegetable Beer',
+    category: 'Spiced Beer',
+    category_id: '30',
+    style_id: '30A'
+  },
+  {
+    name: 'Autumn Seasonal Beer',
+    category: 'Spiced Beer',
+    category_id: '30',
+    style_id: '30B'
+  },
+  {
+    name: 'Winter Seasonal Beer',
+    category: 'Spiced Beer',
+    category_id: '30',
+    style_id: '30C'
+  },
+  {
+    name: 'Specialty Spice Beer',
+    category: 'Spiced Beer',
+    category_id: '30',
+    style_id: '30D'
+  },
+  {
+    name: 'Alternative Grain Beer',
+    category: 'Alternative Fermentables Beer',
+    category_id: '31',
+    style_id: '31A'
+  },
+  {
+    name: 'Alternative Sugar Beer',
+    category: 'Alternative Fermentables Beer',
+    category_id: '31',
+    style_id: '31B'
+  },
+  {
+    name: 'Classic Style Smoked Beer',
+    category: 'Smoked Beer',
+    category_id: '32',
+    style_id: '32A'
+  },
+  {
+    name: 'Specialty Smoked Beer',
+    category: 'Smoked Beer',
+    category_id: '32',
+    style_id: '32B'
+  },
+  //  {
+  //    name: 'Wood-Aged Beer',
+  //    category: 'Wood Beer',
+  //    category_id: '33',
+  //    style_id: '33A'
+  //  },
+  //  {
+  //    name: 'Specialty Wood-Aged Beer',
+  //    category: 'Wood Beer',
+  //    category_id: '33',
+  //    style_id: '33B'
+  //  },
+  {
+    name: 'Commercial Specialty Beer',
+    category: 'Specialty Beer',
+    category_id: '34',
+    style_id: '34A'
+  },
+  {
+    name: 'Mixed-Style Beer',
+    category: 'Specialty Beer',
+    category_id: '34',
+    style_id: '34B'
+  },
+  {
+    name: 'Experimental Beer',
+    category: 'Specialty Beer',
+    category_id: '34',
+    style_id: '34C'
+  },
+  {
+    name: 'Dorada Pampeana',
+    category: 'Argentine Styles',
+    category_id: 'X',
+    style_id: 'X1'
+  },
+  {
+    name: 'IPA Argenta',
+    category: 'Argentine Styles',
+    category_id: 'X',
+    style_id: 'X2'
+  },
+  //'Argentine IPA',
+  {
+    name: 'Italian Grape Ale',
+    category: 'Italian Styles',
+    category_id: 'X',
+    style_id: 'X3'
+  },
+  {
+    name: 'Catharina Sour',
+    category: 'Brazilian Styles',
+    category_id: 'X',
+    style_id: 'X4'
+  },
+  {
+    name: 'New Zealand Pilsner',
+    category: 'New Zealand Styles',
+    category_id: 'X',
+    style_id: 'X5'
+  }
 ]
 
 const flattenKeys = require('./flatter').flattenKeys
@@ -605,7 +773,11 @@ async function go() {
 
           if (collect && value != styleName) {
             console.log(collect, param, key, value)
-            if (value.trim().endsWith(':') && value != 'that are either:') {
+            if (
+              value.trim().endsWith(':') &&
+              value != 'that are either:' &&
+              value != 'Saison, Session Strength, Comment: '
+            ) {
               param = _.snakeCase(value.trim().replace(':', ''))
               result[param] = ''
             } else {
